1d827b13e04edd32aafa3282ece991ffaa07cae8
Dlugosi <32090657+Dlugosi@users.noreply.github.com>
2018-01-12 20:52:26 +0100

    Update CrearGraf1.cpp

--- CrearGraf1.cpp
+++ CrearGraf1.cpp
@@ -15,17 +15,12 @@
 
 using namespace std;
 
-int sink;
-int source;
-int supersource;
-int supersink;
 
 struct edge{    //es un vol
     int final;  //al que esta conectat
-    int min;
-    int max;
+    int capacitat;
+	int flow;
     bool usat;
-    int ant; //per l'algorisme
 };
 
 int main ()
@@ -33,7 +28,7 @@ int main ()
     ifstream input;
     input.open ("inputs/instance_100_2_1.air");
     if(not input.is_open()) cout << "no s'ha trobat el fitxer" << endl;
-
+	
 	vector < vector < int > > Entrada; 
 	string line;
 	while (not input.eof())
@@ -49,41 +44,50 @@ int main ()
             Viatge[2] = line[4] - '0';   //Entrada[x][2] es el temps de sortida
             // cin >> num;
             Viatge[3] = line[6] - '0';   //Entrada[x][3] es le temps de arrivada
-		//for	(int i=0; i<Viatge.size();i++) cout << Viatge[i] << "  ";
-		//cout << endl;
+			//for	(int i=0; i<Viatge.size();i++) cout << Viatge[i] << "  ";
+			//cout << endl;
             Entrada.push_back (Viatge);
         }
     }   //<O(n);
     input.close();
-                            
+	
     int size = Entrada.size();
-    vector < vector < edge> > Graf (2*size+2);
+    vector < vector < edge> > Graf (2*size+4);
     sink = (2*size)+1;
     source = 0;
     supersource = sink*1;
     supersink = supersource*1
-
+	
     for(int i=0; i<size; i++){  
-		//conectem el font als origens
+		//conectem el source als origens
         edge nova;
         nova.final = i+1;
-        nova.min = 0;
-        nova.max = 1;
-        nova.ant = 0;
+        nova.capacitat = 1;
+        nova.flow = 0;
         nova.usat = false;
-        Graf[0].push_back(nova);
-		//conectem els origens als destins
-        nova.final = i+size+1;
-        nova.min = 1;
-        nova.max = 1;
-        nova.ant = i+1;
+        Graf[source].push_back(nova);
+		//no conectem els origens als destins, sino que els transformem en sinks i sources per complir
+		//el lower bound (no queda aresta). Per fer aixo cal un super sink i un super source que rep i dona tot el fluxe
+	    //primer els nous sink(origens) al supersink (falta el sink original) 
+		
+        nova.final = supersink;
+        nova.capacitat = 1;
+        nova.flow = 0;
         nova.usat = false;
         Graf[i+1].push_back(nova);
+		
+	    //ara el supersource als destins falta conectar al source
+		
+		nova.final = i+size+1;
+        nova.capacitat = 1;
+        nova.flow = 0;
+        nova.usat = false;
+        Graf[supersource].push_back(nova);
+		
 		//conectem els destins al sink
         nova.final = sink;
-        nova.min=0;
-        nova.max=1;
-        nova.ant = i+1+size;
+        nova.capacitat=1;
+        nova.flow = 0;
         nova.usat = false;
         Graf[i+size+1].push_back(nova);
     }                           //O(n)
@@ -92,18 +96,66 @@ int main ()
     
     for(int i = 0; i<size; i++){
         for (int j=0; j<size; j++){
-
+			
             if ((Entrada[i][3]+15<Entrada[j][2])&&(Entrada[i][1]==Entrada[j][0])){
                 edge nova;
                 nova.final=j+1;
-                nova.min=0;
-                nova.max=1;
+                nova.capacitat=1;
+				nova.flow=0;
                 nova.ant = i+size+1;
                 nova.usat = false;
                 Graf[i+size+1].push_back(nova);
             }
         }
     }               //O(n^2)
+	
+	/*for (int i=0; i<Graf.size(); i++) {
+	 for (int j=0; j<Graf[i].size(); j++) {
+	 cout << Graf[i][j].final <<" ";
+	 }
+	 cout << endl;
+	 }*/
+	bool primer = true;
+	int Pmin=0;
+	int Pmax=size;
+	int millorflow;
+	while(Pmax > Pmin){
+		primer = false;
+
+		int valor = (Pmin+Pmax)/2;
+		
+		edge nova;
+        nova.final = supersink;
+        nova.capacitat = valor;
+        nova.flow = 0;
+        nova.usat = false;
+		if (primer){
+			Graf[sink].push_back(nova);//conectem sink a supersink
+		}
+		else {
+			Graf[sink][Graf[sink].size()-1] = nova;
+		}
+		edge nova;
+        nova.final = source;
+        nova.capacitat = valor;
+        nova.flow = 0;
+        nova.usat = false;
+		if (primer){
+			Graf[supersource].push_back(nova);//conectem sink a supersink
+		}
+		else {
+			Graf[supersource][Graf[supersource].size()-1] = nova;
+		}
+				
+		if (flow(Graf, valor)) { //aixo cal canviarho
+			Pmax = valor -1;//no volem tornar a provar aquest
+			millorflow = valor;
+			//guardar resposta
+		}
+		else {
+			Pmin = valor+1;
+			
+		}          //O(n^2)
 	
 	/*for (int i=0; i<Graf.size(); i++) {
 		for (int j=0; j<Graf[i].size(); j++) {
